0e5c6a0f66e6663faf8941f8814d6b3bb7b8aa99
gituser6hij <user137@protonmail.com>
2023-03-15 11:31:22 +0700

    checktest3

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -3,7 +3,7 @@ import styles from "../../styles/Navbar.module.css";
 export default function Navbar() {
 	return (
 		<nav className={styles.navbar}>
-			<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
+			<a href="http://web3.auditutils.com" target={"_self"}>
 				<img
 					className={styles.alchemy_logo}
 					src="/alchemy_logo.svg"
