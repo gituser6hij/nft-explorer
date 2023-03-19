de31e0b407744f8412c35348d825720c17ed2ee4
gituser6hij <user137@protonmail.com>
2023-03-20 01:27:40 +0700

    bg-color 1c1c1c

---
+++ .next/static/e9X1L1JBVWs6t0s8m5K1Y/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/d19b0fd418e2fb63.css","static/chunks/pages/index-f2efc4274183a462.js"],"/_error":["static/chunks/pages/_error-409f831d3504c8f5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
