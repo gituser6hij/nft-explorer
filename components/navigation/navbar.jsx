bc0530039bb74f51a0e195af5da38e9e276e0d1d
gituser6hij <user137@protonmail.com>
2023-03-23 00:22:30 +0700

    re next link bg color

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -3,7 +3,7 @@ import styles from "../../styles/Navbar.module.css";
 export default function Navbar() {
 	return (
 		<nav className={styles.navbar}>
-			<a href="http://web3.auditutils.com" target={"_self"}>
+			<a href="https://user137-portfolio.auditutils.com" target={"_self"}>
 				<img
 					className={styles.alchemy_logo}
 					src="/user137.PNG"
